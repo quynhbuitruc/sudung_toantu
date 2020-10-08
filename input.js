58a763cd519d9935bcc5ee8615c1ce3288734e4a
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-09 00:03:32 +0900

    tinh dien tich

--- input.js
+++ input.js
@@ -2,3 +2,5 @@ let width = prompt ("Nhập chiều rộng:");
 let height = prompt ("Nhập chiều dài");
 let Width = parseInt ('width')
 let Height = parseInt ('height')
+let area = Width * Height
+document.write("The area is: " + area);
